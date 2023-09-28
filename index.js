a62b276cdfa0584af0c7550500b89453bf69d263
Li2206 <lievinbisangwa@gmail.com>
2023-09-28 11:22:48 +0200

    Data_generator

---
+++ index.js
@@ -0,0 +1,3 @@
+let myMessages =['Salut l\'ami(e)', 'Tu vas bien?', 'Je suis un admirateur des sciences humaines',' Je suis aussi un amateur du numérique','j\'espère que tu t\'y connais aussi en informatique', 'Je reve de rencontrer "Neil Degrace Tayson"'];
+let someHobbies = ['je fais du sport', 'Je lis tout type de livre ouvert au monde réel', 'je suis un grand reveur']
+let otherTricks = ['Les voyages à travers le monde, me tentent','Je suis un citoyen du monde', 'En vrai','je suis consmopolite']
