2b775a0209f141ab0fcbff8146a76e078581760e
christian birego <93344052+birego@users.noreply.github.com>
2024-03-11 13:44:22 +0100

    projet twitter

---
+++ twitter-clone/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Timeline from './Components/Timeline'
+
+
+function App() {
+
+  return (
+    <>
+    
+      <Timeline />
+      
+    </>
+  )
+}
+
+export default App
